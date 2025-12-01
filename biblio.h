c8abf0f95c996c4bd7c5c8057c6205af369dc491
Hamshigaa.J <155365917+hamshij05@users.noreply.github.com>
2025-12-01 12:45:16 +0100

    Define usine and AVL tree structures in biblio.h
    Added structures for usine and AVL tree for water distribution.

--- biblio.h
+++ biblio.h
@@ -7,3 +7,22 @@
 #include <string.h>
 #include <math.h>
 #include <limits.h>
+
+//Struture de l'usine
+typedef struct usine {
+char identifiant;
+
+//Stucture de la distribution principale (jonctions)
+typedef struct {
+char*
+//Struture d'Arbre AVL
+typedef struct avl{
+
+Traitement : usine de traiteent
+Stockage : 
+Distribution : principale (jonctions), secondaire (raccordements), branchement (relier le réseaux avec le compteur d'eau de l'usager)
+Usagers : plus de vingaine de million
+
+Seuls les trançons de raccordement utilises  par notre programme
+Trançons (captage, stockage, distribution principale, secondaire, branchement) 
+Les infos de chaque usine de traitement (caacité maximale annuelle de traitement de l'eau)
